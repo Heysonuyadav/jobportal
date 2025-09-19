99ab418ea02bc6d8d4fbc54024a8f711f92d24e3
Heysonuyadav <yadavjisonu488@gmail.com>
2025-09-19 13:37:52 +0530

    Fix: correct Table import in ApplicantsTable.jsx

--- frontend/src/components/ApplicantsTable.jsx
+++ frontend/src/components/ApplicantsTable.jsx
@@ -1,5 +1,5 @@
 import { Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover"
-import {Table,TableBody,TableCaption,TableCell,TableHead,TableHeader,TableRow,} from "@/components/ui/table.jsx"
+import {Table,TableBody,TableCaption,TableCell,TableHead,TableHeader,TableRow,} from "@/components/ui/Table.jsx"
 import { MoreHorizontal } from 'lucide-react'
 import { useSelector } from "react-redux";
 import axios from "axios";
