8c933c64a67d800f9d4fba504d070a0be33b51b8
sonu yadav <yadavjisonu488@gmail.com>
2025-11-22 13:22:20 +0530

    Update API endpoints to use production URLs

--- frontend/src/uttils/constant.jsx
+++ frontend/src/uttils/constant.jsx
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT="http://localhost:3000/user";
-export const JOB_API_END_POINT="http://localhost:3000/jobRoutes";
-export const APPLICATION_JOB_END_POINT="http://localhost:3000/applicant";
-export const COMPANIES_JOB_END_POINT="http://localhost:3000/company";
+export const USER_API_ENDPOINT="https://jobportal-2-9txp.onrender.com/user";
+export const JOB_API_END_POINT="https://jobportal-2-9txp.onrender.com/jobRoutes";
+export const APPLICATION_JOB_END_POINT="https://jobportal-2-9txp.onrender.com/applicant";
+export const COMPANIES_JOB_END_POINT="https://jobportal-2-9txp.onrender.com/company";
  
